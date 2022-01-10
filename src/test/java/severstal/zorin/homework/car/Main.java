64f179523c07a1527dd2972b882bf12fa09d21f9
ArtemZorin36 <artem-zorin@bk.ru>
2022-01-10 09:04:35 +0300

    добавлены тесты домашнего задания

---
+++ src/test/java/severstal/zorin/homework/car/Main.java
@@ -0,0 +1,21 @@
+package severstal.zorin.homework.car;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car myCar = new Car("zaporozhets", 1.0);
+
+        myCar.automaticTransmission = true;
+        myCar.setColor("red");
+
+        Wheels round = new Wheels("round");
+        Wheels squer = new Wheels("Squer");
+
+        myCar.wheels(round);
+        myCar.showCar();
+
+
+
+    }
+}
---
+++ src/test/java/severstal/zorin/homework/car/Wheels.java
@@ -0,0 +1,10 @@
+package severstal.zorin.homework.car;
+
+public class Wheels {
+
+    String wheelsType;
+
+    public Wheels(String wheelsType) {
+        this.wheelsType = wheelsType;
+    }
+}
