3eafbb981238215a2d44883e0ba2be7a367a16c8
ArtemZorin36 <artem-zorin@bk.ru>
2022-01-10 08:49:27 +0300

    добавлены тесты домашнего задания

---
+++ src/test/java/severstal/zorin/homework/softassert/TestDragAndDrop.java
@@ -0,0 +1,26 @@
+package severstal.zorin.homework.softassert;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestDragAndDrop {
+
+    @BeforeAll
+    static void beforeAll() {
+        clearBrowserCookies();
+        Configuration.browserSize = "1980x1280";
+        Configuration.holdBrowserOpen = false;
+    }
+
+    @Test
+    public void moveSquareTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("b"));
+        $("#column-b").shouldHave(text("a"));
+    }
+}
---
+++ src/test/java/severstal/zorin/homework/softassert/TestSoftAssert.java
@@ -0,0 +1,29 @@
+package severstal.zorin.homework.softassert;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestSoftAssert {
+
+    @BeforeAll
+    static void beforeAll() {
+        clearBrowserCookies();
+        Configuration.browserSize = "1980x1280";
+        Configuration.holdBrowserOpen = false;
+    }
+
+    @Test
+    public void checkPagesSoftAssertionsInGithub() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
+        $("[data-filterable-for=wiki-pages-filter]").shouldHave(text("SoftAssertions"));
+        $("[data-filterable-for='wiki-pages-filter']").$(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
+    }
+}
