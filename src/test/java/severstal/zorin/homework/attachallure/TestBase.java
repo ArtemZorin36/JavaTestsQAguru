ea74f555490b4a0b3564a294bc6c8676c333416e
ArtemZorin36 <artem-zorin@bk.ru>
2022-01-30 14:18:25 +0300

    добавлена домашка №10

--- src/test/java/severstal/zorin/homework/attachallure/TestBase.java
+++ src/test/java/severstal/zorin/homework/attachallure/TestBase.java
@@ -1,5 +1,6 @@
 package severstal.zorin.homework.attachallure;
 
+import com.codeborne.selenide.Configuration;
 import io.qameta.allure.Step;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -15,9 +16,20 @@ public class TestBase {
     @Step("Настройки браузера")
     static void beforeAll() {
         baseUrl = "https://demoqa.com";
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "91");
+        String remoteUrl = System.getProperty("remoteUrl", "selenoid.autotests.cloud/wd/hub1");
+        String login = System.getProperty("login", "user1");
+        String pass = System.getProperty("pass", "1234");
+
+        baseUrl = "https://demoqa.com";
         browserSize = "1920x1080";
-        browser = System.getProperty("browser");
-        browserVersion = System.getProperty("browserVersion");
+        Configuration.browser = browser;
+        browserVersion = version;
+
+        String url = "https://" + login + ":" + pass + "@" + remoteUrl;
+
+        remote = url;
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
